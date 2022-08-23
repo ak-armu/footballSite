128aeb470ca349e72343c76204ae0d09a3ff1690
Abdur Karim Arman <armanbd.cse@gmail.com>
2022-08-23 21:08:18 +0600

    project ready

--- dom.js
+++ dom.js
@@ -1,35 +1,103 @@
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    const playerCostField = document.getElementById('player-cost');
-    const playerCostString = playerCostField.value;
-    const playerCost = parseFloat(playerCostString);
+var selectedPlayers = [];
 
-    const playerExpense = document.getElementById('player-total-cost');
-    const playerTotalCost = playerExpense.innerText;
-    playerExpense.innerText = playerCost;
-})
+document.querySelectorAll( 'button.selectPlayer' ).forEach( function( player )
+{
+    player.addEventListener( 'click', function( e )
+    {
+        if ( selectedPlayers.length > 4 )
+        {
+            alert( 'You have already selected ' + selectedPlayers.length + ' players' ); return;
+        }
 
+        const childs = e.target.parentNode.children;
 
+        const playerName = childs[2].innerText;
+        
+        selectedPlayers.push( { playerId : e.target.id, playerName : playerName } );
+
+        e.target.innerText = 'Selected';
+
+        e.target.disabled = true;
+
+        updatetotalSelectedPlayer();
+
+    }, false );
+});
+
+function updatetotalSelectedPlayer()
+{
+    document.getElementById( 'totalSelectedPlayer' ).innerText = selectedPlayers.length;
+
+    const playerLists = document.getElementById( 'player-list' );
+
+    playerLists.innerHTML = "";
+
+    selectedPlayers.forEach( function( player )
+    {
+        var li = document.createElement( "li" );
+    
+        li.appendChild( document.createTextNode( player.playerName ) );
+        
+        playerLists.appendChild( li );
+    } );
+}
+
+document.getElementById( 'calculate-btn' ).addEventListener( 'click', function()
+{
+    if ( ! selectedPlayers.length )
+    {
+        alert( 'Please select players first' ); return;
+    }
+
+    const playerCostVal = document.getElementById( 'player-cost' ).value;
+
+    if ( playerCostVal == '' )
+    {
+        alert( 'Please input per player cost first' ); return;
+    }
+    
+    const playerCost = parseFloat( playerCostVal );
+
+    document.getElementById( 'player-total-cost' ).innerText = playerCost * selectedPlayers.length;
+});
 
 
 // Total Calculation part
+document.getElementById( 'total-calculate-btn' ).addEventListener( 'click', function()
+{
+    if ( ! selectedPlayers.length )
+    {
+        alert( 'Please select players first' ); return;
+    }
 
-document.getElementById('total-calculate-btn').addEventListener('click', function(){
-    const managerCostField = document.getElementById('manager-cost');
-    const managerCostString = managerCostField.value;
-    const managerCost = parseFloat(managerCostString);
+    const playerCostVal = document.getElementById( 'player-total-cost' ).innerText;
 
-    const coachCostField = document.getElementById('coach-cost')
-    const coachCostString = coachCostField.value;
-    const coachCost = parseFloat(coachCostString);
+    if ( playerCostVal == '' )
+    {
+        alert( 'Please input per player cost first' ); return;
+    }
 
-    const playerExpense = document.getElementById('player-total-cost');
-    const playerTotalCostString = playerExpense.innerText;
-    const playerCost = parseFloat(playerTotalCostString);
+    const playerCost = parseFloat( playerCostVal );
 
-    const managerAndCoachCost = managerCost + coachCost + playerCost;
+    const managerCostVal = document.getElementById( 'manager-cost' ).value;
 
-    const totalCalculation = document.getElementById('total-cost');
-    const totalCost = totalCalculation.innerText;
-    totalCalculation.innerText = managerAndCoachCost;
+    if ( managerCostVal == '' )
+    {
+        alert( 'Please input manager cost first' ); return;
+    }
 
-})
+    const managerCost = parseFloat( managerCostVal );
+
+    const coachCostVal = document.getElementById( 'coach-cost' ).value;
+
+    if ( coachCostVal == '' )
+    {
+        alert( 'Please input coach cost first' ); return;
+    }
+
+    const coachCost = parseFloat( coachCostVal );
+
+    const PlayerManagerAndCoachCost = playerCost + managerCost + coachCost;
+
+    document.getElementById( 'total-cost' ).innerText = PlayerManagerAndCoachCost;    
+});
